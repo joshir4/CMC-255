5b5f0d3862ad5cee7223db8d2465fb21ff361773
joshir4 <69922033+joshir4@users.noreply.github.com>
2020-08-20 16:09:32 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/************************************************************
+* Hello.java
+*************************************************************
+* Raj Joshi
+* 08/19/2020
+* CMSC 255 and C90
+*************************************************************/
+
+public class Hello {
+
+	public static void main ( String [] args) {
+		
+		System.out.println("Raj Joshi");
+		System.out.println("Hello.java");
+		System.out.println("Project Description: To make sure Java is properly installed and loaded on your computer and that you can run a program in Java.");
+		System.out.println("08/19/2020");
+		System.out.println("CMSC 255 C90");
+		System.out.println();
+		}
+	}
